2bf52beb96a08204b7de3ddcc204a8375727969f
Augustin Floren <augustinfloren@gmail.com>
2024-09-25 16:29:45 +0200

    Structure + header, hero, section1

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,17 +16,34 @@ module.exports = {
           dark: '#0062ff',
         }
       },
+      spacing: {
+        btn: '8px',
+        title: '50px', 
+        card: '34px',
+        badge: '3px',
+        section: '104px',
+        "mobile-title": '36px',
+        "mobile-card": '21px',
+        "mobile-btn": "9px",
+      },
       fontSize: {
-        sm: '0.75rem',
-        base: '1rem',
-        medium: '1.777rem',
-        large: '2.369rem',
-        xl: '5.61rem',
-        
+        sm: '0.8rem',
+        base: '1.25rem',
+        medium: '1.563rem',
+        large: '1.953rem',
+        xl: '3.815rem',
+        "mobile-sm": '1rem',
+        "mobile-base": '1.2rem',
+        "mobile-medium": '1.44rem',
+        "mobile-large": '1.728rem',
+        "mobile-xl": '2.986rem',
       },
       fontFamily: {
         display: 'bricolage grotesque, sans-serif',
         body: 'inter, sans-serif',
+      },
+      lineHeight: {
+        'title': '48px',
       }
     },
   },
