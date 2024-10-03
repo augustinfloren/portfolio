58f971c752a2374df46a7b85ef72aaad12107266
Augustin Floren <augustinfloren@gmail.com>
2024-10-03 16:10:26 +0200

    Contenu en place

--- tailwind.config.js
+++ tailwind.config.js
@@ -22,6 +22,7 @@ module.exports = {
         card: '34px',
         badge: '3px',
         section: '72px',
+        large: '100px',
         'mobile-section': '52px',
         'mobile-title': '36px',
         'mobile-card': '21px',
@@ -29,16 +30,15 @@ module.exports = {
       },
       fontSize: {
         badge: '0.3rem',
+        xs: 'clamp(0.3rem, 0.971rem + -0.19vw, 0.8rem)',
         sm: 'clamp(0.9rem, 0.971rem + -0.19vw, 0.8rem)',
         base: 'clamp(1rem, 0.821rem + 0.476vw, 1.25rem)',
         medium: 'clamp(1.44rem, 1.352rem + 0.234vw, 1.563rem)',
         large: 'clamp(1.424rem, 1.046rem + 1.008vw, 1.953rem)',
         xl: 'clamp(2.986rem, 2.394rem + 1.579vw, 3.815rem)',
-        'mobile-sm': '1rem',
-        'mobile-base': '1.2rem',
-        'mobile-medium': '1.44rem',
-        'mobile-large': '1.728rem',
-        'mobile-xl': '2.986rem',
+      },
+      letterSpacing: {
+        shrink: 'clamp(-0.5rem, -0.2rem, 0)',
       },
       fontFamily: {
         display: 'bricolage grotesque, sans-serif',
